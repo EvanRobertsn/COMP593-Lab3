92268d636b489c81aff150e04df68c9ecd8b7864
EvanRobertsn <evan.robertson@flemingcollege.ca>
2022-06-17 17:06:03 -0400

    Finished lab 3
    Re-read over the instructions, did what had to be done. added comments that aren't too vague. Are you still reading this Jeremy?

--- Comp75 -Lab 3 script.py
+++ Comp75 -Lab 3 script.py
@@ -1,24 +1,24 @@
 #Comp75 -Lab 3 script
-
-
-
+#lists movie genres in data structure
 def list_genre(movies):
     list=[]
+    #creates list of movie genres
     for genre in movies['movies']:
         list.append(genre['genre'])
+    #list all movie genres from list
     for i in range(len(list)):
         if(i==0):
             print("Some of my Fav movies Genres are:",end=" ")
-        
+        #check if the genre is not the last entry
         if(i<len(list)-1):
             
             print(list[i]+',',end=" ")
         else:
             print("and",list[i]+".")
-
+#same as list_genre but for movie titles
 def list_title(titles):
     list=[]
-    for title in titles['movies']:
+    for title in titles:
         list.append(title['title'])
     for i in range(len(list)):
         if(i==0):
@@ -29,34 +29,30 @@ def list_title(titles):
             print(list[i]+',',end=" ")
         else:
             print("and",list[i]+".")
-
-def list_toppings(toppings,case_cos):
-    print("My fav Pizza Toppings are:")
+#lists the toppings on my pizza
+def list_toppings(toppings):
+    print("My favourite Pizza Toppings are:")
     for top in toppings['pizza_toppings']:
-        if(case_cos==False):
-            print("-",top)
-        elif(case_cos==True):
-            print("-",top.lower())
-
-def stud_name_id(full_name,stud_id):
-    first_name=full_name.split( )
-    print("My name is",first_name[0],",But you can call me Lord",full_name,"\n")
-    print("My Student ID is",stud_id,"\n")
-    pass
-
+         print("-",top)
+#lists my name and student ID
+def stud_name_id(db):
+    first_name=db['full_name'].split( )
+    print("My name is",first_name[0],",But you can call me Lord",db['full_name'],"\n")
+    print("My Student ID is",db['student_id'],"\n")
+#adds topping to my pizza topping list from a tuple
 def add_piz_top(pizza, top_tup):
     pizza['pizza_toppings']+=top_tup
     pizza['pizza_toppings'].sort()
-    case_cos=True
-    return pizza,case_cos
-    pass
+    for i in range(len(pizza['pizza_toppings'])):
+        pizza['pizza_toppings'][i]=pizza['pizza_toppings'][i].lower()
+    return pizza
 
 #create empty main function w/ pass call
 def main():
 
     #create dictonary w/ information
     hackers_dream={
-        "full_name": "Evan Robertson",
+        "full_name": "CLARENCE BEEFTANK",
 
         "student_id": 69420,
 
@@ -83,16 +79,22 @@ def main():
 
     'genre': "Animated Comedy"
     }
+    #add new movie to the dictonary
     hackers_dream['movies'].append(new_movie)
     pizza_top_tuple=("Raw Chicken","The biG toe fRom the right foot of your firstborn","Olives")
-    case_constant=False
-    #function Calls
-    stud_name_id(hackers_dream['full_name'],hackers_dream["student_id"])
+    
+    #function Calls within main
+    stud_name_id(hackers_dream)
+    
     list_genre(hackers_dream)
-    list_title(hackers_dream)
-    list_toppings(hackers_dream,case_constant)
-    hackers_dream,case_constant=add_piz_top(hackers_dream,pizza_top_tuple)
-    list_toppings(hackers_dream,case_constant)
+
+    list_title(hackers_dream['movies'])
+
+    list_toppings(hackers_dream)
+    #add pizza toppings to data structure
+    hackers_dream=add_piz_top(hackers_dream,pizza_top_tuple)
+
+    list_toppings(hackers_dream)
 
 main()
 
